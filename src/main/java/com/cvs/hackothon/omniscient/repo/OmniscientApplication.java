f7a4cda403eddf005077f098330b38efb3e31b4b
LAKSHMAN REDDY <lakhs32@gmail.com>
2020-01-09 10:41:21 -0600

    Intial Set up

---
+++ src/main/java/com/cvs/hackothon/omniscient/repo/OmniscientApplication.java
@@ -0,0 +1,13 @@
+package com.cvs.hackothon.omniscient.repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniscientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniscientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvs/hackothon/omniscient/repo/OmniscientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvs.hackothon.omniscient.repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniscientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
